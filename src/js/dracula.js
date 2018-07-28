1ecd28e4ee93dad8fd3af805196b3e0e48325605
Philippe Partarrieu <ppartarr@outlook.com>
2018-07-28 14:03:58 +0100

    fixed custom color background closes #6

--- src/js/dracula.js
+++ src/js/dracula.js
@@ -9,12 +9,25 @@ var createStylesheetElement = function(){
 	link.id = "draculaMessenger";
 	link.href = a;
 
-	return link;
+	return link
 }
 
 if (document.getElementById('draculaMessenger')){
-	console.log("Already exists.");	
+	console.log("Theme is already active.");	
 } else {
+	// check if empty before hand to avoid error
+	// add custom colour to messages sent by me
+	var myMessages = document.getElementsByClassName("_3i_m");
+	var customColour = myMessages[myMessages.length - 1].getAttribute("threadcustomcolor");
+
+	myMessages = Array.prototype.slice.call(myMessages);
+	myMessages.map(message => message.style.setProperty('--custom-colour', customColour));
+
+	// add custom colour to text next to icons in Conversation Information > Options
+	var sharedFiles = document.getElementsByClassName("_fy2");
+	sharedFiles = Array.prototype.slice.call(sharedFiles);
+	sharedFiles.map(file => file.style.setProperty('--custom-colour', customColour));
+
 	let draculaStylesheet = createStylesheetElement();
 	document.head.appendChild(draculaStylesheet);
     chrome.storage.sync.set({ activated : true }, function(){
